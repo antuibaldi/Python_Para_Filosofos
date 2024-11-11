981b970ede6e821bab1d6fdd540ff52a45b51d10
Antu <122485403+antuibaldi@users.noreply.github.com>
2024-11-11 19:07:24 -0300

    11-11-2024

---
+++ programa4.py
@@ -0,0 +1,4 @@
+filosofia = False
+
+while filosofia:
+    print ("La filosofia es la madre de todas la ciencias")
---
+++ programa5.py
@@ -0,0 +1,5 @@
+frase = input ("Dame tu nombre y te dire cuantas letras tiene")
+
+total_caracteres = len (frase)
+
+print (total_caracteres)
